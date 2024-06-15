7aedec7547df72f93736c0bbfa78cd70f33dff19
7Flafleur <7nb@hotmail.fr>
2024-06-15 21:18:13 +0200

    eslint flowtype error

--- Front/react-analytics-dashboard/src/components/App.js
+++ Front/react-analytics-dashboard/src/components/App.js
@@ -2,29 +2,21 @@
 
 import Main from "./Main";
 import Errrorpage from "./Errorpage"
-import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 
 
 
 export default function App() {
 
-
-
-
-
-
   return (
-
-   
-
 
     <div className="App">
 
       <Router>
 
-       
+
         <Routes>
-          <Route path="/" element={<Errrorpage/>}/>
+          <Route path="/" element={<Errrorpage />} />
           <Route path="/user/:id" element={<Main />} />
           <Route path="*" element={<Errrorpage />} />
         </Routes>
--- Front/react-analytics-dashboard/src/components/Averagesessions.jsx
+++ Front/react-analytics-dashboard/src/components/Averagesessions.jsx
@@ -11,7 +11,7 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-let xValue=null;
+let xValue = null;
 
 
 const CustomTooltip = ({ active, payload, label }) => {
@@ -30,98 +30,98 @@ const CustomTooltip = ({ active, payload, label }) => {
 
 
 
-export default function Averagesessions(props){
+export default function Averagesessions(props) {
 
 
-  const [activeIndex,setActiveindex] = useState(7)
+  const [activeIndex, setActiveindex] = useState(7)
 
-  function MouseOverState(state){
-     if(state.isTooltipActive){
+  function MouseOverState(state) {
+    if (state.isTooltipActive) {
       const index = sessions.findIndex(session => session.day === state.activeLabel);
 
       setActiveindex(index)
-     }
-     else{
+    }
+    else {
       setActiveindex(7)
-     }
- 
+    }
+
   }
 
 
-console.log("Session props:", props.sessions)
+  console.log("Session props:", props.sessions)
 
-const sessions = props.sessions.map((item) => {
-  let day;
-  switch(item.day){
-    case 1:
-      day = 'L';
-      break;
-    case 2:
-      day = 'M';
-      break;
-    case 3:
-      day = 'M';
-      break;
-    case 4:
-      day = 'J';
-      break;
-    case 5:
-      day = 'V';
-      break;
-    case 6:
-      day = 'S';
-      break;
-    case 7:
-      day= 'D'
-      break;
-    default:
-      day = item.day;
-  }
-  return {...item, day};
-});
+  const sessions = props.sessions.map((item) => {
+    let day;
+    switch (item.day) {
+      case 1:
+        day = 'L';
+        break;
+      case 2:
+        day = 'M';
+        break;
+      case 3:
+        day = 'M';
+        break;
+      case 4:
+        day = 'J';
+        break;
+      case 5:
+        day = 'V';
+        break;
+      case 6:
+        day = 'S';
+        break;
+      case 7:
+        day = 'D'
+        break;
+      default:
+        day = item.day;
+    }
+    return { ...item, day };
+  });
 
-const minVal = Math.min(...sessions.map(item => item.sessionLength));
-const maxVal = Math.max(...sessions.map(item => item.sessionLength));
-
-
-console.log("active index ",activeIndex )
+  const minVal = Math.min(...sessions.map(item => item.sessionLength));
+  const maxVal = Math.max(...sessions.map(item => item.sessionLength));
 
 
+  console.log("active index ", activeIndex)
 
 
-    return(
 
-      
-        <div className="averagesessions">
-        <ResponsiveContainer className="responsivecontainer" width="100%" height="100%">
+
+  return (
+
+
+    <div className="averagesessions">
+      <ResponsiveContainer className="responsivecontainer" width="100%" height="100%">
         <p className='durée'>Durée moyenne des séances</p>
 
-          <LineChart width="100%" height="100%"
-            data={sessions} 
-            onMouseMove={MouseOverState}
-            onMouseLeave={() => setActiveindex(7)}
-          >
-         
-         <XAxis dataKey="day" axisLine={false} stroke='#fff' opacity={0.7} tickLine={false} tickMargin={-20} padding={{ left: 10, right: 10 }} />
-            <YAxis domain={[minVal - 20, maxVal + 20]} hide={true} />
-            <Tooltip content={<CustomTooltip  />} />  
-            <Line type="monotone" dataKey="sessionLength" stroke="#ffffff" dot={{ r: 0 }} activeDot={{ r: 3 }} />
-            <CartesianGrid stroke="#fff" opacity={0.5} horizontal={false} /> {/* Add this line */}
+        <LineChart width="100%" height="100%"
+          data={sessions}
+          onMouseMove={MouseOverState}
+          onMouseLeave={() => setActiveindex(7)}
+        >
 
-              <ReferenceArea className='referencearea'
-                x1={activeIndex}
-                x2={6}
-                strokeOpacity={0.8}
-           
-                
-                
-               
-                fill="#979797"
-              />
-            
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
-    )
+          <XAxis dataKey="day" axisLine={false} stroke='#fff' opacity={0.7} tickLine={false} tickMargin={-20} padding={{ left: 10, right: 10 }} />
+          <YAxis domain={[minVal - 20, maxVal + 20]} hide={true} />
+          <Tooltip content={<CustomTooltip />} />
+          <Line type="monotone" dataKey="sessionLength" stroke="#ffffff" dot={{ r: 0 }} activeDot={{ r: 3 }} />
+          <CartesianGrid stroke="#fff" opacity={0.5} horizontal={false} /> {/* Add this line */}
+
+          <ReferenceArea className='referencearea'
+            x1={activeIndex}
+            x2={6}
+            strokeOpacity={0.8}
+
+
+
+
+            fill="#979797"
+          />
+
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
